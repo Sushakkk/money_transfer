b0548597dc7b5869695fa25c782613b4d3eec31b
Карпова Ксения <ksenia.karpova.4@mail.ru>
2024-08-13 23:51:21 +0300

    скролл

--- js/script.js
+++ js/script.js
@@ -1,35 +1,25 @@
 const burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.header__menu');
-
 const main_menu = document.querySelector('.menu');
-
-
+const body = document.querySelector('body'); // Добавьте эту строку, чтобы определить body
 
 main_menu.addEventListener('click', (e) => {
-    if (e.target.classList.contains('menu__link') || (e.target.classList.contains('button_main'))) {
+    if (e.target.classList.contains('menu__link') || e.target.classList.contains('button_main')) {
         burger.classList.toggle('active'); //при нажатии убирает или добавляет 
         menu.classList.toggle('active');
-        body.classList.toggle('lock')
+        body.classList.toggle('lock');
     }
-
 });
-
-
-
-
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active'); //при нажатии убирает или добавляет 
     menu.classList.toggle('active');
-    body.classList.toggle('lock')
-
+    body.classList.toggle('lock');
 });
-
 
 // подчеркивание активной ссылки в навигации
 document.querySelector('.menu__list').addEventListener('click', function(e) {
     if (e.target.classList.contains('menu__link')) {
-        e.preventDefault(); // Предотвращаем стандартное действие (перезагрузку страницы)
         document.querySelectorAll('.menu__link').forEach(link => link.classList.remove('active_link'));
         e.target.classList.add('active_link');
     }
